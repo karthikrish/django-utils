23b860c6b9d5cf1ffc140dc6bfa8c782384293c8
Charles Leifer <coleifer@gmail.com>
2011-03-09 00:42:56 -0600

    Call super __init__()

--- djutils/db/managers.py
+++ djutils/db/managers.py
@@ -1,11 +1,12 @@
-from django.db.models import Manager
+from django.db import models
 
 from djutils.constants import LIVE_STATUS
 
 
-class PublishedManager(Manager):
-    def __init__(self, status_field='status'):
+class PublishedManager(models.Manager):
+    def __init__(self, status_field='status', *args, **kwargs):
         self.status_field = status_field
+        super(PublishedManager, self).__init__(*args, **kwargs)
     
     def published(self):
         return self.filter(**{self.status_field: LIVE_STATUS})
