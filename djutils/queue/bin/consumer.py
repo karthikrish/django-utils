4d0ace9cf59da85bb5cc1c1ca83921912a1b23a4
Charles Leifer <coleifer@gmail.com>
2011-03-30 09:30:53 -0500

    Log available commands

--- djutils/queue/bin/consumer.py
+++ djutils/queue/bin/consumer.py
@@ -120,6 +120,10 @@ class QueueDaemon(Daemon):
         self.logger.info('Initializing daemon with options:\npidfile: %s\nlogfile: %s\ndelay: %s\nbackoff: %s\nthreads: %s' % (
             self.pidfile, self.logfile, self.delay, self.backoff_factor, self.threads))
 
+        self.logger.info('Loaded classes:\n%s' % '\n'.join([
+            klass for klass in registry._registry
+        ]))
+
         self.start_workers()
         
         try:
